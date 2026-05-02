d5724478d08320c7739de59d20d18d0587f2bbad
m1kpe0 <yarikasik@inbox.ru>
2026-05-02 21:55:35 +0300

    fixed size dot, improved sizing (auto size)

--- TheWorstUI.lua
+++ TheWorstUI.lua
@@ -195,22 +195,20 @@
                 HolderFrame.Holder.UIListLayout.AbsoluteContentSize.Y + 55, 
                 WindowConfig.SizeY ~= 0 and WindowConfig.SizeY or 50, 
                 WindowConfig.SizeY ~= 0 and WindowConfig.SizeY or 9999
-            )
-            MainWindow.Size = UDim2.new(
-                0, 
-                WindowConfig.SizeX, 
-                0, 
-                SizeY
-            )
+            ); PlayTween(MainWindow, 0.1, {Size = UDim2.new(
+                0, WindowConfig.SizeX, 
+                0, SizeY
+            )})
+
             HolderFrame.Holder.CanvasSize = UDim2.new(0, 0, 0, HolderFrame.Holder.UIListLayout.AbsoluteContentSize.Y)
         end)
 
-        local Opened = true
+        Tab.Opened = true
         AddConnection(TopBar.Button.InputEnded, function(Input)
             if Input.UserInputType ~= Enum.UserInputType.MouseButton1 and Input.UserInputType ~= Enum.UserInputType.Touch then return end
-            Opened = not Opened
+            Tab.Opened = not Tab.Opened
 
-            if not Opened then
+            if not Tab.Opened then
                 local TweenButton = PlayTween(TopBar.Button, 0.1, {
                 Size = UDim2.new(0, 0, 0, 0),
                 Position = UDim2.new(1, -25, 0, 25)
@@ -224,8 +222,10 @@
 
                 local Tween1, Tween2
                 Tween1 = PlayTween(HolderFrame, 0.05, {Size = UDim2.new(1, 10, 1, 10)})
+                PlayTween(FakeResizingFrame, 0.05, {Position = UDim2.new(1, 10, 1, 10)})
                 Tween1.Completed:Once(function() 
                     Tween2 = PlayTween(HolderFrame, 0.1, {Size = UDim2.new(0, 0, 0, 0)}) 
+                    PlayTween(FakeResizingFrame, 0.1, {Position = UDim2.new(1, 0, 1, -MainWindow.AbsoluteSize.Y + 45)})
                     Tween2.Completed:Once(function() HolderFrame.Visible = false end)
                 end)
             else
@@ -242,10 +242,16 @@
 
                 HolderFrame.Visible = true
                 local Tween1, Tween2
+
                 Tween1 = PlayTween(HolderFrame, 0.1, {Size = UDim2.new(1, 0, 1, 0)})
+                PlayTween(FakeResizingFrame, 0.1, {Position = UDim2.new(1, 0, 1, 0)})
                 Tween1.Completed:Once(function() 
                     Tween2 = PlayTween(HolderFrame, 0.05, {Size = UDim2.new(1, 10, 1, 10)}) 
-                    Tween2.Completed:Once(function() PlayTween(HolderFrame, 0.1, {Size = UDim2.new(1, 0, 1, 0)}) end)
+                    PlayTween(FakeResizingFrame, 0.1, {Position = UDim2.new(1, 10, 1, 10)})
+                    Tween2.Completed:Once(function() 
+                        PlayTween(HolderFrame, 0.1, {Size = UDim2.new(1, 0, 1, 0)}) 
+                        PlayTween(FakeResizingFrame, 0.1, {Position = UDim2.new(1, 0, 1, 0)})
+                    end)
                 end)
             end
         end)
@@ -273,7 +279,7 @@
                 UserInputService.InputChanged:Connect(function(Input)
                     if Input == DragInput and Dragging then
                         local Delta = Input.Position - MousePos
-                        PlayTween(Main, {0.3, Enum.EasingStyle.Quint, Enum.EasingDirection.Out}, {
+                        PlayTween(Main, {0.2, Enum.EasingStyle.Quint, Enum.EasingDirection.Out}, {
                             Position  = UDim2.new(FramePos.X.Scale,FramePos.X.Offset + Delta.X, FramePos.Y.Scale, FramePos.Y.Offset + Delta.Y)
                         }):Play()
                     end
@@ -284,16 +290,20 @@
         local function AddResizingFunctionality(ResizePoint, Main)
             pcall(function()  
                 local Dragging, DragInput, MousePos, FrameSize = false
+                local Clamp, Sized = false, false
+                
                 ResizePoint.InputBegan:Connect(function(Input)
                     if Input.UserInputType == Enum.UserInputType.MouseButton1 or Input.UserInputType == Enum.UserInputType.Touch then
                         Dragging = true
                         MousePos = Input.Position
                         FrameSize = Main.Size
+                        Clamp = false
 
                         Input.Changed:Connect(function()
                             if Input.UserInputState == Enum.UserInputState.End then
                                 Dragging = false 
                                 Sizing = false
+                                Clamp = false
                             end
                         end)
                     end
@@ -312,8 +322,36 @@
                         
                         local X, Y = FrameSize.X.Offset, FrameSize.Y.Offset
                         if WindowConfig.CanResize == "BOTH" or WindowConfig.CanResize == "X" then X = math.clamp(FrameSize.X.Offset + Delta.X, 160, 9999) end
-                        if WindowConfig.CanResize == "BOTH" or WindowConfig.CanResize == "Y" then Y = math.clamp(FrameSize.Y.Offset + Delta.Y, 50, 9999) end
+                        if WindowConfig.CanResize == "BOTH" or WindowConfig.CanResize == "Y" then 
+                            if Tab.Opened then Y = FrameSize.Y.Offset + Delta.Y end
+                        end
+
+                        local HolderSizeY = HolderFrame.Holder.UIListLayout.AbsoluteContentSize.Y + 55
+                        local SizeChanged = false
                         
+                        if WindowConfig.CanResize == "BOTH" or WindowConfig.CanResize == "Y" then
+                            if not Clamp then
+                                if math.abs(Y - HolderSizeY) <= 20 then
+                                    Y = HolderSizeY
+                                    Sized = true
+                                    Clamp = true
+                                    SizeChanged = true
+                                else
+                                    Y = math.clamp(Y, 50, 9999)
+                                end
+                            else
+                                if math.abs(Delta.Y) > 30 then
+                                    Sized = false
+                                    Y = math.clamp(HolderSizeY + Delta.Y, 50, 9999)
+                                    Clamp = false
+                                    SizeChanged = true
+                                else
+                                    Y = HolderSizeY
+                                    WindowConfig.SizeY = 0
+                                end
+                            end
+                        end
+
                         local size = UDim2.new(
                             FrameSize.X.Scale, X,
                             FrameSize.Y.Scale, Y
@@ -321,7 +359,10 @@
 
                         WindowConfig.Size = size
                         WindowConfig.SizeX = X
-                        if WindowConfig.CanResize == "BOTH" or WindowConfig.CanResize == "Y" then WindowConfig.SizeY = Y end
+                        if WindowConfig.CanResize == "BOTH" or WindowConfig.CanResize == "Y" then 
+                            if not Sized then WindowConfig.SizeY = Y end 
+                        end
+                        if SizeChanged then FrameSize = size; MousePos = Input.Position end
                     end
                 end)
             end)
